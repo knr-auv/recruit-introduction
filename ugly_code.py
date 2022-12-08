64aec6686e427b385a574eb344609ae0fa477d12
PIPRdestroyer <piotrek14528@gmail.com>
2022-12-09 00:04:00 +0100

    fix code

--- ugly_code.py
+++ ugly_code.py
@@ -1,10 +1,18 @@
-import utils
+"""docstring for module"""
 import numpy as np
 import cv2
 
-def insanely_long_function(who_the_f_needs_so_long_argument_name, and_another_one, next_one_and_still_going, whatadafak, pls_stop_im_done):
-    what_are_we_even_doing= who_the_f_needs_so_long_argument_name+ str( and_another_one ** 2 ) +next_one_and_still_going.upper() + whatadafak + pls_stop_im_done
-    return what_are_we_even_doing 
+def insanely_long_function(
+    who_the_f_needs_so_long_argument_name,
+    and_another_one, next_one_and_still_going,
+    whatadafak, pls_stop_im_done):
+    """This is docstring which describe function"""
+    what_are_we_even_doing= (
+        who_the_f_needs_so_long_argument_name+
+        str( and_another_one ** 2 ) +
+        next_one_and_still_going.upper() +
+        whatadafak + pls_stop_im_done)
+    return what_are_we_even_doing
 
 if __name__ == "__main__":
     print(cv2.__version__, np.__version__)
--- utils.py
+++ utils.py
@@ -2,7 +2,7 @@
 """
 def laugh_of(name: str):
     """insanely complicated function for laughing
-    
+
     Uses state-of-the-art technology to laugh of someone
     """
     print(f"hehe {name}")
