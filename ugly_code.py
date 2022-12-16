44a02f55ce2cd0afeabf0e932ed9831d6173ba71
maciej.morawski <62252131+mpfmorawski@users.noreply.github.com>
2022-12-16 15:38:26 +0100

    refactor: Solving long parameter list code smell in insanely_long_function

--- ugly_code.py
+++ ugly_code.py
@@ -1,23 +1,32 @@
+from dataclasses import dataclass
+
 import cv2
 import numpy as np
 
-import utils
+
+@dataclass
+class InsanelyLongFunctionInput:
+    introduction: str
+    number_to_be_squared: int
+    text_to_be_shouted: str
+    expansion: str
+    conlusion: str
 
 
-def insanely_long_function(
-    who_the_f_needs_so_long_argument_name, and_another_one, next_one_and_still_going, whatadafak, pls_stop_im_done
-):
+def insanely_long_function(text_input: InsanelyLongFunctionInput):
     what_are_we_even_doing = (
-        who_the_f_needs_so_long_argument_name
-        + str(and_another_one**2)
-        + next_one_and_still_going.upper()
-        + whatadafak
-        + pls_stop_im_done
+        text_input.introduction
+        + str(text_input.number_to_be_squared**2)
+        + text_input.text_to_be_shouted.upper()
+        + text_input.expansion
+        + text_input.conlusion
     )
     return what_are_we_even_doing
 
 
 if __name__ == "__main__":
-    print(f"OpenCV version: {cv2.__version__}", f"NumPy version: {np.__version__}", sep='\n')
+    print(f"OpenCV version: {cv2.__version__}", f"NumPy version: {np.__version__}", sep="\n")
     test_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    print(insanely_long_function("hehe", "siuuuu", 123132, test_array, "wow!"))
+    print(
+        insanely_long_function(InsanelyLongFunctionInput("hehe", 123132, "siuuuu", np.array2string(test_array), "wow!"))
+    )
