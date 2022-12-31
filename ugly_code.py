c905e9cfc392e688f71eb09e24a2d6f6a22585d2
jakubcieslik <jakub.cieslik1999@gmail.com>
2022-12-31 11:42:06 +0100

    removing comment

--- ugly_code.py
+++ ugly_code.py
@@ -13,4 +13,3 @@ if __name__ == "__main__":
     print(cv2.__version__, np.__version__)
     test_array = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     print(insanely_long_function("hehe", "siuuuu", str(123132), str(test_array), "wow!"))
-# kom
